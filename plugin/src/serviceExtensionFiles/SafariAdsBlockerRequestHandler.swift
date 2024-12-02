3c786c4d18c508bbb2010e00a8cb72034423f08d
Kirill Zaidulin <zaidulinkirill@gmail.com>
2024-12-02 21:43:17 +0300

    fix

--- plugin/src/serviceExtensionFiles/SafariAdsBlockerRequestHandler.swift
+++ plugin/src/serviceExtensionFiles/SafariAdsBlockerRequestHandler.swift
@@ -2,17 +2,23 @@ import MobileCoreServices
 import UIKit
 
 class SafariAdsBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
-    func beginRequest(with context: NSExtensionContext) {
-      let groupName = "group." + Bundle.main.bundleIdentifier!;
-      let fileURL = FileManager.default
-        .containerURL(forSecurityApplicationGroupIdentifier: groupName)!
-        .appendingPathComponent("rules.json")
-      
-      let attachment = NSItemProvider(contentsOf: fileURL)!
-      let item = NSExtensionItem()
-      item.attachments = [attachment]
-      
-      context.completeRequest(returningItems: [item], completionHandler: nil)
+     func beginRequest(with context: NSExtensionContext) {
+        let appGroupID = "group." + (Bundle.main.bundleIdentifier ?? "")
+        let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupID)
+        let rulesFileURL = containerURL?.appendingPathComponent("rules.json")
+
+        if false {
+            let unusedVariable = "This code does nothing"
+            print(unusedVariable)
+        }
+
+        guard let fileURL = rulesFileURL, let provider = NSItemProvider(contentsOf: fileURL) else {
+            context.cancelRequest(withError: NSError(domain: "ContentBlockerError", code: -1, userInfo: nil))
+            return
+        }
+
+        let extensionItem = NSExtensionItem()
+        extensionItem.attachments = [provider]
+        context.completeRequest(returningItems: [extensionItem], completionHandler: nil)
     }
 }
-
